d1b636a02a239901b50dc44ae7cbcbfff2cd1e7f
Qubo Qin <quboqin0710@gmail.com>
2021-08-27 16:08:00 +0800

    add profile view

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,19 @@ import router from './router'
 import './index.css'
 
 import 'vant/lib/index.css'
-import { Search, Tab, Tabs, Field, CellGroup, Button } from 'vant'
+import { Search, Tab, Tabs, Field, CellGroup, Button, Image } from 'vant'
 
 const app = createApp(App)
 
 app.use(router)
 
-app.use(Search).use(Tab).use(Tabs).use(Field).use(CellGroup).use(Button)
+app
+  .use(Search)
+  .use(Tab)
+  .use(Tabs)
+  .use(Field)
+  .use(CellGroup)
+  .use(Button)
+  .use(Image)
 
 app.mount('#app')
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,21 +4,108 @@
       <div>我的</div>
     </template>
   </Header>
-  <div class="mt-14">Profile</div>
+  <div class="mt-14">
+    <div
+      class="
+        my-2
+        mx-4
+        flex
+        items-center
+        justify-between
+        bg-white
+        rounded-md
+        px-2
+      "
+    >
+      <div class="flex items-center justify-start">
+        <van-image
+          class="my-2"
+          round
+          width="3rem"
+          height="3rem"
+          :src="avatarUrl"
+        />
+        <div class="ml-2 text-gray-600 text-left">
+          <div class="font-semibold">QQB</div>
+          <div>ID: weee7343219</div>
+        </div>
+      </div>
+      <p class="mdi mdi-chevron-right text-3xl text-center leading-none"></p>
+    </div>
+    <div class="mx-4 my-3 text-gray-600 bg-white rounded-md">
+      <div class="pl-2 pt-2 text-left">我的订单</div>
+      <hr class="bg-gray-800 my-2" />
+      <div class="flex justify-between py-2 px-4">
+        <div class="" v-for="(type, index) in orderTypes" :key="index">
+          <img class="mx-auto h-8 w-8" :src="orderImages[index]" />
+          <div class="text-center text-xs">
+            {{ type }}
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      class="
+        mx-4
+        my-3
+        text-gray-600
+        bg-white
+        rounded-md
+        flex
+        items-center
+        justify-between
+        p-2
+      "
+    >
+      <div>我的地址</div>
+      <p class="mdi mdi-chevron-right text-3xl text-center leading-none"></p>
+    </div>
+    <div
+      class="
+        mx-4
+        my-3
+        text-gray-600
+        bg-white
+        rounded-md
+        flex
+        items-center
+        justify-between
+        p-2
+      "
+    >
+      <div>我的信用卡</div>
+      <p class="mdi mdi-chevron-right text-3xl text-center leading-none"></p>
+    </div>
+  </div>
   <tab-bar></tab-bar>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive, toRefs } from 'vue'
 
 import Header from '@/components/Header.vue'
 import TabBar from '@/components/TabBar.vue'
+import avatarUrl from '@/assets/avatar.jpg'
+import clock from '@/assets/icons8-clock-64.png'
+import box from '@/assets/icons8-box-64.png'
+import trunk from '@/assets/icons8-boot-open-64.png'
 
 export default defineComponent({
   name: 'Profile',
   components: {
     Header,
     TabBar,
+  },
+  setup() {
+    const state = reactive({
+      avatarUrl,
+      orderTypes: ['全部订单', '待付款', '待发货'],
+      orderImages: [clock, box, trunk],
+    })
+
+    return {
+      ...toRefs(state),
+    }
   },
 })
 </script>
